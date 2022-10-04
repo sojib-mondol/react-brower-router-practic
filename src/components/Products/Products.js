3beed0ee19ca44ebce2cb87b30fffa1224ba3a0f
sojib-sudo <sojibmandol@gmail.com>
2022-10-04 22:59:01 +0600

    react BrowserRouter practic

---
+++ src/components/Products/Products.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Products = () => {
+    return (
+        <div>
+            <h2>Lot's of products here.</h2>
+        </div>
+    );
+};
+
+export default Products;
